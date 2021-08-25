739148c2c343a76de4a957b51bfd6b6a11cd6fd3
Aidan Thorpe <aidant842@gmail.com>
2021-08-25 16:47:09 +0100

    added state and conditional rendering to component and tested possibilties

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,58 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
-test("Renders Hello World! as a text", () => {
-    // Arrange
-    render(<Greeting />);
+// Grouping tests together into suites
+describe("Greeting Component", () => {
+    test("Renders 'Hello World!' as a text", () => {
+        // Arrange
+        render(<Greeting />);
 
-    // Act
-    // .. nothing
+        // Act
+        // .. nothing
 
-    // Assert
-    const helloWorldElement = screen.getByText("Hello World!");
-    expect(helloWorldElement).toBeInTheDocument();
+        // Assert
+        const helloWorldElement = screen.getByText("Hello World!");
+        expect(helloWorldElement).toBeInTheDocument();
+    });
+
+    test("Renders 'It's good to see you!' if button isn't pressed", () => {
+        // Arrange
+        render(<Greeting />);
+
+        // Act
+        // ...nothing
+
+        // Assert
+        const testElement = screen.getByText("It's good to see you!");
+        expect(testElement).toBeInTheDocument();
+    });
+
+    test("Renders 'Changed!' if button is pressed", () => {
+        // Arrange
+        render(<Greeting />);
+
+        // Act
+        const buttonElement = screen.getByRole("button");
+        userEvent.click(buttonElement);
+
+        // Assert
+        const testElement = screen.getByText("Changed!");
+        expect(testElement).toBeInTheDocument();
+    });
+
+    test("Doesn't render 'It's good to see you!' if button is pressed", () => {
+        // Arrange
+        render(<Greeting />);
+
+        // Act
+        const buttonElement = screen.getByRole("button");
+        userEvent.click(buttonElement);
+
+        //Assert
+        const testElement = screen.queryByText("good to see you", {
+            exact: false,
+        });
+        expect(testElement).toBeNull();
+    });
 });
